951a65b8cdb17fac59d3f46d201272af50a7b5a7
samarth088 <skshah987654@gmail.com>
2025-07-13 08:40:13 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const authRoutes = require('./routes/authRoutes');
+const messageRoutes = require('./routes/messageRoutes');
+
+app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log(`Server running on port ${process.env.PORT}`);
+        });
+    })
+    .catch((err) => console.log(err));
